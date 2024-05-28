ed3f7eaceb98148314c89ef1b91b1a4358211864
muhammadzainnurfaid <muhammadzainnurfaid20@gmail.com>
2024-05-28 21:05:26 +0700

    zher

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optional JavaScript for additional interactivity
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Website CV is fully loaded');
+    // You can add more JavaScript functionality here
+});
